03b08bc3345abb426fad5f3da9fb8f3c6b7a70b3
Moe Jangda <moe@squareup.com>
2022-10-07 12:58:16 -0500

    remove parallelization because dwn has no support for it

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
-import cluster from 'node:cluster';
-
-import config from './config';
-
-import { app } from './app';
+import app from './app';
 import { Server } from './server';
 
-const { numProcesses } = config.relay;
 
-if (cluster.isPrimary) {
-  // create DID if not exists
+const server = new Server(app);
 
-  console.log(`[pid:${process.pid}] Primary - Forking ${numProcesses} processes`);
-
-  // event handler that is triggered whenever a worker processor exits
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`[pid:${process.pid}] Primary - pid:${worker.process.pid} died with code ${code}`);
-  });
-
-  for (let i = 0; i < numProcesses; i += 1) {
-    cluster.fork();
-  }
-} else {
-  const server = new Server(app);
-  
-  server.listen(9000, () => {
-    console.log('server listening on port 9000');
-  });
-}
+server.listen(9000, () => {
+  console.log('server listening on port 9000');
+});
