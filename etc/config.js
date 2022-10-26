214da76857475397fbfa24aadfee91a1d0bb14b0
Neal <groessler24@gmail.com>
2022-10-26 18:49:07 -0500

    adding env

--- etc/config.js
+++ etc/config.js
@@ -1,38 +1,38 @@
 export default {
-    relay: {
-      numProcesses : 1,
-      handlers     : [
-        {
-          filter: {
-            method : 'CollectionsQuery',
-            schema : 'https://somehost.com/CredentialManifest'
-          },
-          endpoint: {
-            method : 'GET',
-            url    : 'http://localhost:8080/v1/manifests',
-          },
-          responseMapping: {
-            '200': {
-              schema: 'https://identity.foundation/credential-manifest/schemas/credential-manifest'
-            }
-          }
+  relay: {
+    numProcesses: 1,
+    handlers: [
+      {
+        filter: {
+          method: "CollectionsQuery",
+          schema: "https://somehost.com/CredentialManifest",
         },
-        {
-          filter: {
-            method   : 'CollectionsWrite',
-            protocol : 'https://identity.foundation/decentralized-web-node/protocols/credential-issuance',
-            schema   : 'https://identity.foundation/credential-manifest/schemas/credential-application'
-          },
-          endpoint: {
-            method : 'PUT',
-            url    : 'http://localhost:8080/v1/manifests/applications',
-          },
-          responseMapping: {
-            '201': {
-              schema: 'https://identity.foundation/credential-manifest/schemas/credential-response'
-            }
-          }
+        endpoint: {
+          method: "GET",
+          url: (process.env.API_URL == undefined ? "http://localhost:8080" : process.env.API_URL) + "/v1/manifests",
         },
-      ]
-    }
-  };
+        responseMapping: {
+          200: {
+            schema: "https://identity.foundation/credential-manifest/schemas/credential-manifest",
+          },
+        },
+      },
+      {
+        filter: {
+          method: "CollectionsWrite",
+          protocol: "https://identity.foundation/decentralized-web-node/protocols/credential-issuance",
+          schema: "https://identity.foundation/credential-manifest/schemas/credential-application",
+        },
+        endpoint: {
+          method: "PUT",
+          url: (process.env.API_URL == undefined ? "http://localhost:8080" : process.env.API_URL) + "/v1/manifests/applications",
+        },
+        responseMapping: {
+          201: {
+            schema: "https://identity.foundation/credential-manifest/schemas/credential-response",
+          },
+        },
+      },
+    ],
+  },
+};
